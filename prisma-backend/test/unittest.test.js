6311a65a41a23358d0aa54662af0054a39663396
unknown <shouvik72500@gmail.com>
2021-07-07 21:00:11 +0530

    test case added

--- prisma-backend/test/unittest.test.js
+++ prisma-backend/test/unittest.test.js
@@ -7,50 +7,49 @@ chai.should();
 
 chai.use(chaiHttp);
 
-describe('Tasks API', () => {
+describe('RentDex API', () => {
 
     /**
      * Test the GET route
      */
     describe("GET /api/tasks", () => {
-        it("It should GET all the tasks", (done) => {
+        it("It should GET all the items", (done) => {
             chai.request(server)
                 .get("/items")
                 .end((err, response) => {
                     response.should.have.status(200);
                     response.body.should.be.a('array');
-                    const obj =response.body[0]
-                    obj.should.be.a('object') ;
+                    const obj = response.body[0]
+                    obj.should.be.a('object');
                     obj.should.have.property('id');
                     obj.should.have.property('name');
                     obj.should.have.property('price');
                     obj.should.have.property('description');
                     obj.should.have.property('imgurl');
-                    obj.should.have.property('seller');
-                    obj.should.have.property('category');
-                    obj.should.have.property('selleremail');
-                    
-                    
-                    
-                done();
+                    obj.should.have.property('maxduration');
+                    obj.should.have.property('postedby');
+
+
+
+                    done();
                 });
         });
-        
-        it("It should GET all the tasks", (done) => {
+
+        it("Login Test", (done) => {
             chai.request(server)
-                .get("/login?username=carteasy.user1@gmail.com&password=123")
+                .get("/login?username=test&password=123")
                 .end((err, response) => {
                     response.should.have.status(200);
                     response.body.should.be.a('object');
                     console.log(response.body)
                     // response.body.should.have.property('error');
-                    
-                    
-                    
-                done();
+
+
+
+                    done();
                 });
         });
-        
+
 
     });
 
